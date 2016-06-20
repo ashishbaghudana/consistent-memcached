faca108cfd1329927fd1fa23bfa57850ace3bc27
Ashish Baghudana <abaghudana@paypal.com>
2016-06-20 09:57:27 +0530

    All changes from local

--- setup.py
+++ setup.py
@@ -2,20 +2,19 @@ from setuptools import setup
 from setuptools import find_packages
 
 def readme():
-    with open('README.rst', encoding='utf-8') as file:
+    with open('README.rst') as file:
         return file.read()
 
 setup(
     name='consistent-memcached',
     version='0.1.0',
-    description='Pipeline for NER of natural language mutation mentions',
+    description='Consistent Hashing and Automatic Failover for Memcached',
     long_description=readme(),
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 2.7',
         'Topic :: System :: Distributed Computing',
-        'Topic :: Text Processing :: Linguistic'
     ],
     keywords='consistent hashing memcached automatic failover',
     url='https://github.com/ashishbaghudana/consistent-memcached',
